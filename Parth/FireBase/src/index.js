480984ee9adcbe0fe2b23798430839b0f1653742
TheRoookie <parthbharvadiya@gmail.com>
2024-02-18 21:24:16 +0530

    Basic Data Fetching complete-Firebase

--- Parth/FireBase/src/index.js
+++ Parth/FireBase/src/index.js
@@ -2,8 +2,9 @@ import { initializeApp } from 'https://www.gstatic.com/firebasejs/9.0.0/firebase
 import {
     collection,
     getDocs,
+    doc, getDoc, addDoc, updateDoc, deleteDoc, query, where, orderBy,
     getFirestore,
-    addDoc
+    onSnapshot
 } from 'https://www.gstatic.com/firebasejs/9.0.0/firebase-firestore.js';
 
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
@@ -25,28 +26,65 @@ const db = getFirestore()
 
 var IMEI = ""
 var nodes = new Array()
-var location = ""
+var profile = { hi: "Hello" }
 var users = new Array()
-
+var pass = ""
 
 document.getElementById("loginForm").addEventListener('submit', (event) => {
-    event.preventDefault();
-    alert(IMEI)
+    event.preventDefault()
     IMEI = document.forms["loginForm"]["gwid"].value
     const col = collection(db, IMEI)
 
-    getDocs(col).then((snapshot) => {
+    onSnapshot(col, (snapshot) => {
+
         if (!snapshot.empty) {
-            console.log("Document data: ", snapshot.docs[0].data());
+            nodes = snapshot.docs
+            //get the profile document from col
+            var prof_doc = doc(db, IMEI + "/profile")
+            getDoc(prof_doc).then((profile_doc) => {
+                console.log(profile_doc.data())
+                profile = profile_doc.data()
+
+                let prof_page = document.getElementById("Profile")
+                prof_page.innerHTML = ""
+                let kido = document.createElement('div')
+                kido.innerHTML += "Location : " + profile.location + "<br>"
+                kido.innerHTML += 'User 1 : ' + profile.user1 + '<br>'
+                kido.innerHTML += 'User 2 : ' + profile.user2 + '<br>'
+                prof_page.appendChild(kido)
+            })
 
 
 
 
-            document.getElementById("IMEI").innerHTML += IMEI
+            let node = document.getElementById("nodes")
+            node.innerHTML = ""
+            for (let i = 0; i < nodes.length - 1; i++) {
+                let kid = document.createElement('div')
+
+                kid.innerHTML += ("Node Unique ID : " + nodes[i].id + "<br>")
+                kid.innerHTML += ("Active Time : " + nodes[i].data().activeTime + "<br>")
+                kid.innerHTML += ("Installation Time : " + nodes[i].data().installationTime + "<br>")
+                kid.innerHTML += ("Load : " + nodes[i].data().load + "<br>")
+                kid.innerHTML += ("MacID : " + nodes[i].data().macID + "<br>")
+                kid.innerHTML += ("Update Time : " + nodes[i].data().load + "<br>")
+
+
+
+
+                kid.innerHTML += "<br>*******************<br>"
+                node.appendChild(kid)
+            }
+
+
+
+            document.getElementById("IMEI").innerHTML = IMEI
 
 
         } else {
             alert("IMEI not found!")
         }
     })
+
 })
+
